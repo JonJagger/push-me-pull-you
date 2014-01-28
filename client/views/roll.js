3375d519e32237611b534fd72851debd89f46e1b
Jon Jagger <jon@jaggersoft.com>
2014-01-28 11:38:29 +0000

    deleted dead roll code

--- client/views/roll.js
+++
@@ -1,7 +0,0 @@
-
-Template.roll.events({"click #roll":function () {
-  var ids = getDice(this.gid, this.color).map(function(die) { return die._id; });  
-  _.each(ids, function(id) {
-    Dice.update(id, { $set: { value:rollDie() }});
-  });
-}});
