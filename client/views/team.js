42efc1f6dbec5a705df17004578969a688f0d1c2
Jon Jagger <jon@jaggersoft.com>
2014-01-29 09:45:33 +0000

    split kanban js into own file

--- client/views/team.js
+++ client/views/team.js
@@ -31,47 +31,6 @@ Template.downstreamPortal.toColor = function() {
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-Template.kanban.state = function() {
-  if (this.ones.length === 0)
-    return "is-empty is-in-progress";
-  if (this.ones.length < this.size)
-    return "is-in-progress";
-  if (this.ones.length === this.size)
-    return "is-done";
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-Template.kanban.voidGaps = function() {
-  return nOnes(0);
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// If a story's size is 4 and it has had 4 ones played
-// on it then it has no white gaps.
-// If a story's size is 4 and it has had 3 ones played
-// on it then it has one white gap.
-// see {{#each whiteGaps}} in kanban.html
-//
-Template.kanban.whiteGaps = function() {
-  return nOnes(this.size - this.ones.length);
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// The number of ones that have been played on a story.
-// see kanban.html
-//
-
-Template.kanban.onesColors = function() {
-  if (this.ones == "") {
-    return ","; // HACK, returning "" causes kanban.html to emit ...data-ones>
-  } else {                         // rather than ...data-ones="">
-    return this.ones;              // and I don't know why
-  }
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 Template.team.rendered = function() {
   var team = $(".team");
   var color = team.color();
@@ -92,10 +51,6 @@ Template.team.rendered = function() {
 
 var getKanbansIn = function(gid,teamColor,where) {
   return Kanbans.find({ gid:gid, teamColor:teamColor, at:where });    
-};
-
-var nOnes = function(n) { // eg 3
-  return _(n).times(function() { return 1; });  // eg [1,1,1]
 };
 
 var isOne = function(die) {
