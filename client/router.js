dd072938912016fe323d6de04bfa1bc565f54481
Jon Jagger <jon@jaggersoft.com>
2014-01-30 09:01:22 +0000

    gid retained on home page refresh

--- client/router.js
+++ client/router.js
@@ -1,9 +1,22 @@
 
 Router.map(function() { 
-  this.route('home', { path: '/' });
+  this.route('home', { path: '/',
+    data: function() {
+      return {
+        gid: ""
+      }
+    }
+  });
+
+  this.route('home', { path: '/:gid',
+    data: function() {
+      return {
+        gid: this.params.gid
+      }
+    }
+  });
   
-  this.route('team', {
-    path: '/team/:gid/:color',
+  this.route('team', { path: '/team/:gid/:color',
     data: function() {
       return {
           gid: this.params.gid,
@@ -12,8 +25,7 @@ Router.map(function() {
     }
   });
 
-  this.route('dashboard', {
-    path: '/dashboard/:gid',
+  this.route('dashboard', { path: '/dashboard/:gid',
     data: function() {
       return {
          gid: this.params.gid
