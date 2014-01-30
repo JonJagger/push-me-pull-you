a351e0ff675166ff3d774892b4286b5e6095fc11
Jon Jagger <jon@jaggersoft.com>
2014-01-30 12:46:35 +0000

    deleted dead code

--- client/views/drag_drop.js
+++ client/views/drag_drop.js
@@ -96,8 +96,8 @@ var pulledKanbanDroppedOnPushableKanban = function(event,ui) {
   var pulled = ui.draggable;
   var pushable = $(this);
   
-  //TODO
-
+  //TODO check-sizes...
+  alert("pulled dropped on pushable");
 }
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -131,80 +131,3 @@ var nOnes = function(n) { // eg 3
 };
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-$.fn.hasClass = function(klass) {
-  return this.attr("class").indexOf(klass) !== -1;
-};
-
-$.fn.team = function() {
-  return this.closest(".team");
-};
-
-$.fn.ones = function(/*kanban*/) {  
-  var ones = $(this).data("ones");
-  return (ones === "") ? [ ] : ones.split(",");  
-};
-
-$.fn.size = function(/*kanban*/) {
-  return $(this).data("size");
-};
-
-$.fn.id = function() {
-  return $(this).data("id");  
-};
-
-$.fn.color = function() {
-  var node = this;
-  return _.find(teamColors(), function(color) {
-    return node.hasClass(color);
-  });
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-//dragDropSetup(color,
-//              ".upstream.portal .kanban.is-done",
-//              ".wip",
-//              doneKanbanDroppedOnWip);
-//
-//var doneKanbanDroppedOnWip = function(event, ui) {
-//  var kanban = ui.draggable; 
-//  var wip    = $(this);
-//  var toColor = wip.team().color();
-//  Kanbans.update(kanban.id(), { 
-//    $set: {
-//      ones:[ ],
-//      color:toColor
-//      at:"wip"    
-//    }
-//  });    
-//};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-//dragDropSetup(color,
-//              ".kanban.is-done",
-//              ".downstream.portal .kanban",
-//              doneKanbanDroppedOnEmptyKanban);
-//
-//var doneKanbanDroppedOnEmptyKanban = function(event, ui) {
-//  var doneKanban = ui.draggable;
-//  var emptyKanban = $(this);
-//  var ones = doneKanban.ones();
-//  var newValues =  { ones:ones };
-//  
-  // TODO: what if number of ones being xferred is
-  //       greater than size of kanban being dropped on?
-  //       What happens to "left-over" 1s?
-// 
-//  
-//  if (emptyKanban.ones().length + ones.length === emptyKanban.size()) {
-//    newValues.teamColor = emptyKanban.color();
-//    newValues.at = "upstream";
-//  }
-//  Kanbans.update(emptyKanban.id(), { $set: newValues })  
-//  Kanbans.update(doneKanban.id(), { $set: { ones:[ ] } });  
-//};
-
-
-
