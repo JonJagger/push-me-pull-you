b665d432590112b28cf8c9d2c13725ace87290c5
Jon Jagger <jon@jaggersoft.com>
2014-02-02 10:28:05 +0000

    eack kanban now in own tr-cell

--- client/views/workArea.js
+++ client/views/workArea.js
@@ -1,24 +1,24 @@
 
 Template.workArea.inProgressKanbans = function() {
   return Kanbans.find({
-    gid:this.gid,
-    teamColor:this.color,
+    gid: this.gid,
+    teamColor: this.color,
     state: { $in: [ 'pullable', 'in-progress', 'pushable'] }
   });
 }
 
 Template.workArea.pulledKanbans = function() {
   return Kanbans.find({
-    gid:this.gid,
-    teamColor:this.color,
+    gid: this.gid,
+    teamColor: this.color,
     state: 'pulled'
   });  
 }
 
 Template.workArea.pushedKanbans = function() {
   return Kanbans.find({
-    gid:this.gid,
-    teamColor:this.color,
+    gid: this.gid,
+    teamColor: this.color,
     state: 'pushed'
   });  
 }
