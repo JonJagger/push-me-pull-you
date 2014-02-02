03e8f389ed3727f8c702682ab14fc044fb5888d8
Jon Jagger <jon@jaggersoft.com>
2014-02-02 09:14:48 +0000

    new-kanban button shows count of X-colored kanban across all teams

--- client/views/makeNewKanban.js
+++ client/views/makeNewKanban.js
@@ -1,6 +1,6 @@
 
 Template.makeNewKanban.count = function() {
-  return Kanbans.find({gid:this.gid, teamColor:this.color}).count();
+  return Kanbans.find({gid:this.gid, color:this.color}).count();
 }
 
 Template.makeNewKanban.events({"click input":function() {
